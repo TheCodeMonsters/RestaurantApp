516c5703aa6ddd31ed6fffdfde218f908d51e488
manuelduarte077 <carlosd99m@gmail.com>
2021-08-31 15:59:32 -0600

    finish project

--- RestaurantApp/RestaurantApp.Android/MainActivity.cs
+++ RestaurantApp/RestaurantApp.Android/MainActivity.cs
@@ -3,15 +3,20 @@
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
+using Android.Views;
+using Android.Widget;
 using Android.OS;
 
 namespace RestaurantApp.Droid
 {
-    [Activity(Label = "RestaurantApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "RestaurantApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
